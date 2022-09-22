f01492b2ae98a4ba2ca90a8c007d5e5aa64ae7ea
Steve <stephenkago10@gmail.com>
2022-09-23 01:57:30 +0300

    assign done

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -4,13 +4,40 @@ import { spicyFoods, getNewRandomSpicyFood } from "../data";
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
 
+  
+// adding elements in an array
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+    const newFoodArray = [...foods,newFood];
+    setFoods(newFoodArray);
   }
 
+
+// revoving elements in an array
+  function handleLiClick(id) {
+    const newFoodArray = foods.filter((food) => food.id !== id);
+    setFoods(newFoodArray);
+  }
+
+  //updating elements in an array
+  
+  // function handleLiClick(id) {
+  //   const newFoodArray = foods.map((food) => {
+  //     if (food.id === id) {
+  //       return {
+  //         ...food,
+  //         heatLevel: food.heatLevel + 1,
+  //       };
+  //     } else {
+  //       return food;
+  //     }
+  //   });
+  //   setFoods(newFoodArray);
+  // }
+
+
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li key={food.id} onClick={() => handleLiClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
